594be7daf646fae6fb8be42849582d2c553ab23a
Douglas Guzman <43095613+DouglasG96@users.noreply.github.com>
2022-03-01 14:32:33 -0600

    configuracion de eureka con un cliente

--- microservicios-usuarios/src/main/java/com/curso/microservicios/app/usuarios/MicroserviciosUsuariosApplication.java
+++ microservicios-usuarios/src/main/java/com/curso/microservicios/app/usuarios/MicroserviciosUsuariosApplication.java
@@ -2,7 +2,9 @@ package com.curso.microservicios.app.usuarios;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class MicroserviciosUsuariosApplication {
 
